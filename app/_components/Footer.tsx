d390aa1e02670ebfc31c05eff5cbba971fe8f693
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-04-17 01:26:46 +0200

    Add Footer component to project

---
+++ app/_components/Footer.tsx
@@ -0,0 +1,13 @@
+import { Section } from "./Section";
+
+export const Footer = () => {
+  return (
+    <footer className="bg-card">
+      <Section className="py-8">
+        <p className="text-sm text-muted-foreground">
+          &copy; 2024 Drifa MALOUM
+        </p>
+      </Section>
+    </footer>
+  );
+};
