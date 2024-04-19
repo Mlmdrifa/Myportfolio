ef7044b16a2a41c355473bcb8a6873a0162509ad
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-04-19 22:59:24 +0200

    update Url

--- app/_components/Projects.tsx
+++ app/_components/Projects.tsx
@@ -47,7 +47,7 @@ export default function Projects() {
               "https://upload.wikimedia.org/wikipedia/commons/thumb/8/81/LinkedIn_icon.svg/2048px-LinkedIn_icon.svg.png"
             }
             name={"Drifa Maloum"}
-            url={"https://www.linkedin.com/in/drifa-maloum-077b362a4/"}
+            url={"https://www.linkedin.com/in/drifa-maloum-077b362a4/".toString()}
           />
         </Card>
       </div>
