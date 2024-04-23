e11e1cc3923185b7fa82382ee5686a1e2bc0288a
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-04-23 15:11:35 +0200

    update responsive in hero component

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -14,9 +14,9 @@ import Link from "next/link";
 export const Hero = () => {
   return (
     <Section className="flex max-md:flex-col items-start gap-4">
-      <div className="flex-[3] w-full p-4 flex flex-col gap-2 text-center ...">
+      <div className="flex-[3] w-full p-4 flex flex-col gap-2 text-center">
         <h2 className="font-caption text-5xl ">Drifa MALOUM</h2>
-        <h3 className="text-3xl font-captiont text-gray-300 ">
+        <h3 className="text-3xl font-caption text-gray-300 ">
           Développeuse React / React Native
         </h3>
         <p className="text-base">
@@ -24,10 +24,8 @@ export const Hero = () => {
           <Link href={"https://reactnative.dev/"}>
             <Code className="text-white ">React Native.</Code>
           </Link>
-          Mon parcours est axé sur la création d&apos;expériences utilisateur
-          fluides et engageantes pour les applications mobiles. Mon expérience
-          comprend la création d&apos;applications mobiles visant à offrir des
-          expériences utilisateur fluides et captivantes.
+          Passionnée par la création d&apos;expériences fluides et captivantes
+          pour les applications mobiles.
         </p>
         <p className="mb-1 mt-1 text-center md:text-left">
           Technologies que j&apos;utilise :
@@ -56,7 +54,7 @@ export const Hero = () => {
         N&apos;hésitez pas à explorer mes réalisations et à me contacter pour
         discuter de collaborations ou en savoir plus sur mes compétences
       </div>
-      <div className="flex-[2] md:ml-auto md:mr-0 flex items-center justify-center">
+      <div className="flex-[2] md:ml-auto md:mr-0 flex items-center justify-center w-80 h-80 rounded-full mx-auto">
         <img
           src="https://media.licdn.com/dms/image/D4E03AQEK6Ohy0pCMRA/profile-displayphoto-shrink_400_400/0/1704585988656?e=1718236800&v=beta&t=G-pOosowLK12fU8zbl0oqp4h9STmiuxEjuhjvLK5CSg"
           className="rounded-full object-cover"
