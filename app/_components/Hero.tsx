af9bec28c6003988d51ea046396382bc77f2f880
Drifa <135760500+Mlmdrifa@users.noreply.github.com>
2024-04-23 13:31:56 +0200

    update hero component

--- app/_components/Hero.tsx
+++ app/_components/Hero.tsx
@@ -14,9 +14,9 @@ import Link from "next/link";
 export const Hero = () => {
   return (
     <Section className="flex max-md:flex-col items-start gap-4">
-      <div className="flex-[3] w-full p-4 flex flex-col gap-2 text-center ...">
+      <div className="flex-[1] w-full p-4 flex flex-col gap-2 text-center md:text-left">
         <h2 className="font-caption text-5xl ">Drifa MALOUM</h2>
-        <h3 className="text-3xl font-captiont text-gray-300  ">
+        <h3 className="text-3xl font-captiont text-gray-300 ">
           Développeuse React / React Native
         </h3>
         <p className="text-base">
@@ -30,7 +30,7 @@ export const Hero = () => {
           expériences utilisateur fluides et captivantes.
         </p>
         <p className="mb-1 mt-1">Technologies que j&apos;utilise :</p>
-        <span className="flex space-x-2 inline">
+        <span className="flex flex-wrap justify-center md:justify-start space-x-2 md:space-x-0 md:space-y-2">
           <Code className=" text-white inline-flex items-center gap-1">
             <SiJavascript /> Javascript
           </Code>
@@ -57,7 +57,7 @@ export const Hero = () => {
       <div className="flex-[2] max:md-m-auto ml-auto">
         <img
           src="https://media.licdn.com/dms/image/D4E03AQEK6Ohy0pCMRA/profile-displayphoto-shrink_400_400/0/1704585988656?e=1718236800&v=beta&t=G-pOosowLK12fU8zbl0oqp4h9STmiuxEjuhjvLK5CSg"
-          className=" w-26 max-w-xs max-md-w-56 rounded-full"
+          className="w-26 max-w-xs md:max-w-56 rounded-full mx-auto md:ml-auto"
           alt="Image de Profil"
         />
       </div>
